bb3b23efe877a539bba8548c2b9e8d23e9fc2731
Kenneth Kaiser <kk1995@live.dk>
2022-10-24 15:59:36 +0200

    Copied SumOfRolls from CDIO1

--- SumOfDice.java
+++ SumOfDice.java
@@ -1,2 +1,22 @@
 public class SumOfDice {
+
+    private int sum;
+
+    public void calcSum(RollDice rollDice){ //Calculates sum of int[]
+        sum = 0;
+        for(int i = 0; i < rollDice.getOurRolls().length; i++){
+            sum += rollDice.getOurRolls()[i];
+        }
+    }
+
+
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
 }
