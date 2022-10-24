f76f06255c37e27cf9fd1db03ee4c80e07d22255
Kenneth Kaiser <kk1995@live.dk>
2022-10-24 18:02:53 +0200

    Created 7-12 fields

--- Code/PlayingField/_10WerewolfWall.java
+++ Code/PlayingField/_10WerewolfWall.java
@@ -1,4 +1,22 @@
 package Code.PlayingField;
 
-public class _10WerewolfWall {
+import Code.Languages.Text;
+
+public class _10WerewolfWall extends Field {
+    @Override
+    public int response() {
+        int fieldPoints = -80;
+
+
+        return fieldPoints;
+    }
+
+    @Override
+    public void message() {
+
+        Text text = new Text();
+
+        text.PickLanguage(10);
+
+    }
 }
--- Code/PlayingField/_12Goldmine.java
+++ Code/PlayingField/_12Goldmine.java
@@ -1,4 +1,23 @@
 package Code.PlayingField;
 
-public class _12Goldmine {
+import Code.Languages.Text;
+
+public class _12Goldmine extends Field {
+    @Override
+    public int response() {
+
+        int fieldPoints = 650;
+
+
+        return fieldPoints;
+    }
+
+    @Override
+    public void message() {
+
+        Text text = new Text();
+
+        text.PickLanguage(12);
+
+    }
 }
