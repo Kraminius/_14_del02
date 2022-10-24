c1b74ec9f7108e395b4ce9d373defca743961e81
Kenneth Kaiser <kk1995@live.dk>
2022-10-24 15:58:51 +0200

    Copied RollDice from CDIO1

--- RollDice.java
+++ RollDice.java
@@ -1,2 +1,27 @@
+import java.util.Random;
+
 public class RollDice {
+
+    private int []ourRolls; //The collected rolls
+
+    private final int numberOfRolls = 2; //Number of times the die is being rolled
+    private Die die = new Die();
+    public void Roll(){
+        ourRolls = new int[numberOfRolls];
+        Random random = new Random();
+
+        for (int i = 0;i<numberOfRolls;i++){ //Runs loop a number of times equal to number of rolls.
+            ourRolls[i] = random.nextInt((die.getMaximum()- die.getMinimum())+1)+1; //Rolls the die, and get a random number between min and max
+
+        }
+    }
+
+    public int[] getOurRolls() {
+        return ourRolls;
+    }
+
+    public void setOurRolls(int[] ourRolls) {
+        this.ourRolls = ourRolls;
+    }
+
 }
