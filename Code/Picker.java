df81a1b4f33340db1dd5a567eec56c1f6a922cf6
Kenneth Kaiser <kk1995@live.dk>
2022-10-24 18:15:15 +0200

    Created all cases in Picker. Still needs to change how to handle points.

--- Code/Picker.java
+++ Code/Picker.java
@@ -1,7 +1,6 @@
 package Code;
 
-import Code.PlayingField.Field;
-import Code.PlayingField._2Tower;
+import Code.PlayingField.*;
 
 public class Picker {
 
@@ -20,6 +19,67 @@ public class Picker {
                 System.out.println(field.response());
                 field.message();
                 break;
+            case 3:
+                field = new _3Crater();
+
+                System.out.println(field.response());
+                field.message();
+                break;
+            case 4:
+                field = new _4PalaceGates();
+
+                System.out.println(field.response());
+                field.message();
+                break;
+            case 5:
+                field = new _5ColdDesert();
+
+                System.out.println(field.response());
+                field.message();
+                break;
+            case 6:
+                field = new _6WalledCity();
+
+                System.out.println(field.response());
+                field.message();
+                break;
+            case 7:
+                field = new _7Monastery();
+
+                System.out.println(field.response());
+                field.message();
+                break;
+            case 8:
+                field = new _8BlackCave();
+
+                System.out.println(field.response());
+                field.message();
+                break;
+            case 9:
+                field = new _9HutsInTheMountain();
+
+                System.out.println(field.response());
+                field.message();
+                break;
+            case 10:
+                field = new _10WerewolfWall();
+
+                System.out.println(field.response());
+                field.message();
+                break;
+            case 11:
+                field = new _11ThePit();
+
+                System.out.println(field.response());
+                field.message();
+                break;
+            case 12:
+                field = new _12Goldmine();
+
+                System.out.println(field.response());
+                field.message();
+                break;
+
         }
 
 
