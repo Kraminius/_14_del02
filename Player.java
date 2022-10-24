1de60a6f76d00d906284d0edadb12e30012d43dd
Kenneth Kaiser <kk1995@live.dk>
2022-10-24 15:57:01 +0200

    Copied Player from CDIO1

--- Player.java
+++ Player.java
@@ -1,5 +1,21 @@
 public class Player {
 
-    //Hej
+    private int number; //The individual players score and number variables.
+    private int score;
+    public Player(int playerNumber){ //The Game.Players class calls this to set the player number.
+        number = playerNumber;
+    }
+    public int getNumber() {
+        return number;
+    }
 
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
