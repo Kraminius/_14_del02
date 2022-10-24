356352188856cda935f7458f6d88bf4105adc66a
AWG96 <s221094@student.dtu.dk>
2022-10-24 18:03:09 +0200

    Added code to _6WalledCity

--- Code/PlayingField/_6WalledCity.java
+++ Code/PlayingField/_6WalledCity.java
@@ -1,4 +1,23 @@
 package Code.PlayingField;
 
-public class _6WalledCity {
+import Code.Languages.Text;
+
+public class _6WalledCity extends Field {
+    @Override
+    public int response() {
+        int fieldPoints = +180;
+
+
+
+
+
+        return fieldPoints;
+    }
+
+    @Override
+    public void message() {
+        Text text = new Text();
+
+        text.PickLanguage(6);
+    }
 }
