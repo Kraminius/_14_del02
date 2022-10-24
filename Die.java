e30a776e1fc85188acaea1acb9e068d37803dfc7
Kenneth Kaiser <kk1995@live.dk>
2022-10-24 15:56:12 +0200

    Copied die from CDIO1

--- Die.java
+++ Die.java
@@ -1,2 +1,15 @@
 public class Die {
+
+    private final int maximum = 6;
+    private final int minimum = 1;
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+
 }
