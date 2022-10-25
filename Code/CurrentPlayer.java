ac8302f7ae06b91c4f99d8706399202bf419e797
Kenneth Kaiser <kk1995@live.dk>
2022-10-25 03:28:03 +0200

    Copied CurrentPlayer code from CDIO1

--- Code/CurrentPlayer.java
+++ Code/CurrentPlayer.java
@@ -1,4 +1,52 @@
 package Code;
 
 public class CurrentPlayer {
+    private int currentPlayerNumberVar;
+    private int currentPlayerNumber;
+
+
+    public CurrentPlayer(){
+
+
+        currentPlayerNumberVar = 1;
+        currentPlayerNumber = 1;
+
+    }
+
+    public void checkCurrentPlayer(Players players){
+
+        if(currentPlayerNumberVar < players.getAllPlayers().length){
+
+            currentPlayerNumberVar += 1;
+            currentPlayerNumber = 1;
+
+        }
+        else {
+
+
+            currentPlayerNumberVar = 1;
+            currentPlayerNumber =2;
+
+
+        }
+
+
+    }
+
+    public int getCurrentPlayerNumberVar() {
+        return currentPlayerNumberVar;
+    }
+
+    public void setCurrentPlayerNumberVar(int currentPlayerNumberVar) {
+        this.currentPlayerNumberVar = currentPlayerNumberVar;
+    }
+
+    public int getCurrentPlayerNumber() {
+        return currentPlayerNumber;
+    }
+
+    public void setCurrentPlayerNumber(int currentPlayerNumber) {
+        this.currentPlayerNumber = currentPlayerNumber;
+    }
 }
+
