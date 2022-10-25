9909578c3901059dca2f321fdd3bfc8be2e9af84
Kenneth Kaiser <kk1995@live.dk>
2022-10-25 15:47:01 +0200

    Created extraturn and wincon logic

--- Code/Game.java
+++ Code/Game.java
@@ -11,15 +11,21 @@ public class Game {
 
         Players players = new Players(numberOfPlayers);
         CurrentPlayer currentPlayer = new CurrentPlayer();
+        Conditions conditions = new Conditions();
 
         do{
 
             currentPlayer.checkCurrentPlayer(players);
-            StartPlayerTurn();
-            TurnProcess(players.getAllPlayers()[currentPlayer.getCurrentPlayerNumber()-1]);
+            do{
+
+                StartPlayerTurn();
+                TurnProcess(players.getAllPlayers()[currentPlayer.getCurrentPlayerNumber()-1], conditions);
+
+            }while(conditions.isExtraTurn());
 
 
-        }while(true);
+
+        }while(!conditions.isWinTerm());
 
 
 
@@ -34,7 +40,7 @@ public class Game {
 
     }
 
-    public void TurnProcess(Player player){
+    public void TurnProcess(Player player, Conditions conditions){
 
 
         //Roll dice
@@ -47,13 +53,23 @@ public class Game {
 
         //Finds the field
         Picker picker = new Picker();
+
+
+
         picker.LandedField(sumOfDice);
+
+
 
         //Calculates player total points
         HandlePoints handlePoints = new HandlePoints();
         handlePoints.GivePlayerPoints(player, picker.getTurnPoints());
 
         System.out.println(player.getNumber() + " " + player.getScore());
+
+        //run conditions
+        conditions.CheckExtraTurn(picker);
+        conditions.WonGame(player);
+
 
 
 
