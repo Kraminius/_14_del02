5bf9b90fc770583be660ea2f8d615b6c38c15854
mikkelJurs <s224279@student.dtu.dk>
2022-10-25 16:09:42 +0200

    Created Display Class for text on screen.

--- Code/Display.java
+++ Code/Display.java
@@ -2,4 +2,33 @@ package Code;
 
 //The base-code is copied from earlier project made by same group. Modified slightly to work in this project.
 public class Display {
+    public void startText(Player player, Conditions conditions) {
+        if (conditions.isExtraTurn()) {
+            System.out.println();
+            System.out.println("Click something to roll!");
+        }
+        else {
+            System.out.println();
+            System.out.print("Game.Player " + player.getNumber() +
+                    " turn." + " Your current points are: " + player.getScore() + ". Click something to roll!");
+            System.out.println();
+        }
+
+    }
+
+    public void turnText(Player player, SumOfDice sumOfDice, Conditions conditions) {
+        System.out.println("Player: " + player.getNumber() + " rolled " + sumOfDice.getSum());
+    }
+    public void endTurnText(Player player) {
+        System.out.println("Your score is now: " + player.getScore() +"!");
+    }
+    public void extraTurnText(Conditions conditions) {
+        if (conditions.isExtraTurn()) {
+            System.out.println("Because you feel energized you got an extra turn, roll again!");
+        }
+        else {
+            System.out.println("Your turn is over!");
+
+        }
+    }
 }
